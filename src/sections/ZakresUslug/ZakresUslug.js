616ddc3e048eb946c916a2e6894769c5921f848b
mazur <and.mazur5@gmail.com>
2020-10-29 20:23:07 +0100

    Fourth Commit

--- src/sections/ZakresUslug/ZakresUslug.js
+++ src/sections/ZakresUslug/ZakresUslug.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Zakresy} from './Zakresy/Zakresy'
 
-const numbers = [1, 2, 3, 4, 5];
-const letters = ["a", "b","c"];
-const pralina = ['lubie cuksy', 'psicholog']
+
 
 const ZakresUslug = () => {
+
+    const numbers = [1, 2, 3, 4, 5];
+    const letters = ["a", "b","c"];
+    const pralina = ['lubie cuksy', 'psicholog']
+    
     return(
         <div className="zakres_background">
             <h1>Usługi</h1>
@@ -13,10 +16,12 @@ const ZakresUslug = () => {
                 Specjalizuję się w udzielaniu wsparcia psychologicznego osobom będącym w trudnej sytuacji życiowej i rodzinnej, zmagającym się z lękiem i obniżonym nastrojem. Szczególnie pochylam się nad aspektem doświadczania kryzysów, zmian, z którymi ciężko sobie poradzić, problemów emocjonalnych, relacyjnych oraz szeroko rozumianej przemocy.
                 Towarzyszę w odzyskiwaniu równowagi emocjonalnej oraz poczucia kontroli nad własnym życiem.
             </h2>
-            <Zakresy className="fas fa-book-open" text={numbers}/>
-            <Zakresy className="fas fa-folder-open" text={letters}/>
-            <Zakresy className="fas fa-book-open" text={pralina} />
-            <Zakresy className="fas fa-folder-open" />
+            <div className="test">
+
+            <Zakresy className="" text={numbers}/>
+            <Zakresy className="" text={letters}/>
+            <Zakresy className="" text={pralina} />
+            </div>
         </div>
     )
     
--- src/sections/ZakresUslug/Zakresy/Zakresy.js
+++ src/sections/ZakresUslug/Zakresy/Zakresy.js
@@ -1,17 +1,14 @@
 import React from 'react';
-// import {ZakresyText} from './ZakresyText'
 
-// const numbers = [1, 2, 3, 4, 5];
-// const letters = ["a", "b","c"];
 
 const Zakresy = ({className, text}) => {
     return(
         <div className="zakresy">
-            <i class={className}></i>
+            <i className={className}></i>
             <div>
-            <ul >
-                {text && text.map((x) =>
-                <li>{x}</li>
+            <ul className="zakresy_list">
+                {text && text.map((item) =>
+                <li>{item}</li>
                 )}
             </ul>
         </div>
